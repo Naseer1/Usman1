364e62978e662f72657698a0de2c7ac47827b6f9
Mano <Mano@Acer-PC.attlocal.net>
2017-04-21 01:37:12 -0500

    Test java commit and push

---
+++ Hellow world/src/ExamleFor.java
@@ -0,0 +1,17 @@
+
+public class ExamleFor {
+
+	public static void main(String[] args) {
+		// for initial value
+		for(int i = 0; i<= 10; i++){
+			System.out.println("i=" +i);
+			}
+	//adding numbers datatype whol number or integer
+		int x = 10;
+		int y = 6;
+		int result = x + y;
+		System.out.println("Result = " +result);
+
+	}
+
+}
